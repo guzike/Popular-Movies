9ccbdc5f588716ad9830b9b2cb85946d5c3e9e9e
Eugene Guzik <guzik.evgeny@gmail.com>
2015-10-08 17:36:02 +0300

    wip

--- app/src/main/java/ua/com/elius/eugene/popularmovies/GalleryFragment.java
+++ app/src/main/java/ua/com/elius/eugene/popularmovies/GalleryFragment.java
@@ -1,15 +1,19 @@
 package ua.com.elius.eugene.popularmovies;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
@@ -24,7 +28,7 @@ public class GalleryFragment extends Fragment {
 
     public final String LOG_TAG = this.getClass().getSimpleName();
 
-    ArrayAdapter<ImageView> mGalleryAdapter;
+    ArrayAdapter mGalleryAdapter;
     String mResponse;
     ArrayList<String> postersRefs;
 
@@ -53,29 +57,23 @@ public class GalleryFragment extends Fragment {
             e.printStackTrace();
         }
 
-        //Prepare adapter for the gallery
-        mGalleryAdapter = new ArrayAdapter<>(getActivity(), R.layout.gallery_cell, R.id.image_cell, postersRefs);
+        //preparing gallery
+        GridView gridview = (GridView) rootView.findViewById(R.id.gallery_grid);
+        gridview.setAdapter(new ImageAdapter(getActivity()));
 
-        GridView gallery = (GridView) rootView.findViewById(R.id.gallery_grid);
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(getActivity(), "" + position,
+                        Toast.LENGTH_SHORT).show();
 
-        gallery.setAdapter(mGalleryAdapter);
-
-        ImageView test_image = (ImageView) rootView.findViewById(R.id.test_image);
-        Picasso.with(rootView.getContext()).load("http://i.imgur.com/DvpvklR.png").into(test_image);
+//                Intent intent = new Intent(getActivity(), Intent.class).putExtra(Intent.EXTRA_TEXT, position);
+//                startActivity(intent);
+            }
+        });
 
         return rootView;
 
-//        forecastList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                String forecast = mForecastAdapter.getItem(position);
-//
-//                Intent intent = new Intent(getActivity(), DetailActivity.class).putExtra(Intent.EXTRA_TEXT, forecast);
-//                startActivity(intent);
-//            }
-//        });
-//
-//        return rootView;
     }
 
     public ArrayList<String> getPosters(String string) throws JSONException {
@@ -94,5 +92,71 @@ public class GalleryFragment extends Fragment {
         }
 
         return posters;
+    }
+
+    public class ImageAdapter extends BaseAdapter {
+        private Context mContext;
+
+        public ImageAdapter(Context c) {
+            mContext = c;
+        }
+
+        public int getCount() {
+            return mThumbIds.length;
+        }
+
+        public Object getItem(int position) {
+            return null;
+        }
+
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        // create a new ImageView for each item referenced by the Adapter
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView;
+            if (convertView == null) {
+                // if it's not recycled, initialize some attributes
+                imageView = new ImageView(mContext);
+//                imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+//                imageView.setPadding(8, 8, 8, 8);
+            } else {
+                imageView = (ImageView) convertView;
+            }
+            Picasso.with(getContext()).load("http://i.imgur.com/DvpvklR.png").into(imageView);
+
+//            imageView.setImageResource(mThumbIds[position]);
+
+            return imageView;
+        }
+
+//        @Override
+//        public void getView(int position, View convertView, ViewGroup parent) {
+//            SquaredImageView view = (SquaredImageView) convertView;
+//            if (view == null) {
+//                view = new SquaredImageView(context);
+//            }
+//            String url = getItem(position);
+//
+//            Picasso.with(context).load(url).into(view);
+//        }
+
+        // references to our images
+        private Integer[] mThumbIds = {
+                R.drawable.sample_2, R.drawable.sample_3,
+                R.drawable.sample_4, R.drawable.sample_5,
+                R.drawable.sample_6, R.drawable.sample_7,
+                R.drawable.sample_0, R.drawable.sample_1,
+                R.drawable.sample_2, R.drawable.sample_3,
+                R.drawable.sample_4, R.drawable.sample_5,
+                R.drawable.sample_6, R.drawable.sample_7,
+                R.drawable.sample_0, R.drawable.sample_1,
+                R.drawable.sample_2, R.drawable.sample_3,
+                R.drawable.sample_4, R.drawable.sample_5,
+                R.drawable.sample_6, R.drawable.sample_7
+        };
     }
 }
