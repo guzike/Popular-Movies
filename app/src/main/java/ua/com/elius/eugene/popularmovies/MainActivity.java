246d2c5f8b1c8c874d35f0832af137842dde7381
Eugene Guzik <guzik.evgeny@gmail.com>
2015-10-20 17:09:07 +0300

    wip

--- app/src/main/java/ua/com/elius/eugene/popularmovies/MainActivity.java
+++ app/src/main/java/ua/com/elius/eugene/popularmovies/MainActivity.java
@@ -1,6 +1,7 @@
 package ua.com.elius.eugene.popularmovies;
 
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
 
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
--- app/src/main/java/ua/com/elius/eugene/popularmovies/SettingsActivity.java
+++ app/src/main/java/ua/com/elius/eugene/popularmovies/SettingsActivity.java
@@ -2,13 +2,15 @@ package ua.com.elius.eugene.popularmovies;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 
 public class SettingsActivity extends PreferenceActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
 
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
     }
-
 
 }
