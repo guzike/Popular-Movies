504df2758596d902c4bd95b14e74d72e36dd0d39
Eugene Guzik <guzik.evgeny@gmail.com>
2015-10-21 14:35:30 +0300

    working

--- app/src/main/java/ua/com/elius/eugene/popularmovies/SettingsActivity.java
+++ app/src/main/java/ua/com/elius/eugene/popularmovies/SettingsActivity.java
@@ -1,14 +1,21 @@
 package ua.com.elius.eugene.popularmovies;
 
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
 
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.preferences);
+        setContentView(R.layout.settings_gallery);
+
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.settings_container, new SettingsFragment())
+                    .commit();
+        }
 
         PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
     }
