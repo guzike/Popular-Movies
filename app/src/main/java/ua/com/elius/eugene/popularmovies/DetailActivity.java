727e8c5160780a9135622b9134731380306b534a
Eugene Guzik <guzik.evgeny@gmail.com>
2015-10-21 16:58:48 +0300

    add: DetailActivity, DetailFragment

---
+++ app/src/main/java/ua/com/elius/eugene/popularmovies/DetailActivity.java
@@ -0,0 +1,17 @@
+package ua.com.elius.eugene.popularmovies;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DetailActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(android.R.id.content, new DetailFragment())
+                    .commit();
+        }
+    }
+}
---
+++ app/src/main/java/ua/com/elius/eugene/popularmovies/DetailFragment.java
@@ -0,0 +1,20 @@
+package ua.com.elius.eugene.popularmovies;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DetailFragment extends Fragment {
+
+    public DetailFragment(){
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
