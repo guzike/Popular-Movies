ff91d50fbb75025c790c903a28b1feb116325382
Eugene Guzik <guzik.evgeny@gmail.com>
2015-12-04 17:25:10 +0200

    MovieColumns edited

--- app/src/main/java/ua/com/elius/eugene/popularmovies/data/MovieColumns.java
+++ app/src/main/java/ua/com/elius/eugene/popularmovies/data/MovieColumns.java
@@ -7,10 +7,41 @@ import net.simonvt.schematic.annotation.PrimaryKey;
 
 public interface MovieColumns {
 
-    @DataType(DataType.Type.INTEGER) @PrimaryKey
+    @DataType(DataType.Type.INTEGER)
+    @PrimaryKey
     @AutoIncrement
     String _ID = "_id";
 
-    @DataType(DataType.Type.TEXT) @NotNull
-    String TITLE = "title";
+    @DataType(DataType.Type.TEXT)
+    @NotNull
+    String BACKDROP_PATH = "backdrop_path";
+
+    @DataType(DataType.Type.INTEGER)
+    @NotNull
+    String ID = "id";
+
+    @DataType(DataType.Type.TEXT)
+    @NotNull
+    String ORIGINAL_TITLE = "original_title";
+
+    @DataType(DataType.Type.TEXT)
+    @NotNull
+    String OVERVIEW = "overview";
+
+    @DataType(DataType.Type.TEXT)
+    @NotNull
+    String RELEASE_DATE = "release_date";
+
+    @DataType(DataType.Type.TEXT)
+    @NotNull
+    String POSTER_PATH = "poster_path";
+
+    @DataType(DataType.Type.REAL)
+    @NotNull
+    String POPULARITY = "popularity";
+
+    @DataType(DataType.Type.REAL)
+    @NotNull
+    String VOTE_AVERAGE = "vote_average";
+
 }
